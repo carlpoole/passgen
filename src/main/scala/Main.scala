091c04f2436a58fd3c6ffce2a20a51834dd705a1
Carl Poole <mail@carlpoole.com>
2015-03-06 00:34:19 -0500

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,32 @@
+import scala.util.Random
+import scala.io.Source
+
+/**
+ * Created by carl on 3/5/15.
+ */
+object Main {
+  def main(args: Array[String]) {
+    println(printableChar())
+    println(upperChar())
+    println(lowerChar())
+    println(digit())
+    println(symbol("!#$^%+@*&"))
+    println(word(7)+word(5)+digit()+digit()+digit())
+  }
+
+  def printableChar():        Char = Random.nextPrintableChar()
+  def upperChar():            Char = (Random.nextInt(26) + 65).asInstanceOf[Char]
+  def lowerChar():            Char = upperChar().toLower
+  def digit():                Int  = Random.nextInt(10)
+  def symbol(chars: String):  Char = chars.charAt(Random.nextInt(chars.length))
+
+  def word(length: Int = 4):  String = {
+    val words = Source.fromFile("/usr/share/dict/words")
+      .mkString
+      .split("\n")
+      .filter(x => x.length() == length)
+
+    words(Random.nextInt(words.length))
+  }
+
+}
