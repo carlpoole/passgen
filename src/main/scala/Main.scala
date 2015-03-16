4beb096dd92cdc1266e59ecd649e6f5b2ed9cc8b
Carl Poole <mail@carlpoole.com>
2015-03-15 23:18:43 -0400

    Added random probability function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,31 @@ import scala.io.Source
  * Created by carl on 3/5/15.
  */
 object Main {
+
   def main(args: Array[String]) {
-    println(printableChar())
-    println(upperChar())
-    println(lowerChar())
-    println(digit())
-    println(symbol("!#$^%+@*&_-"))
-    println(word(5)+word(8)+digit()+digit()+digit())
+//    println(printableChar())
+//    println(upperChar())
+//    println(lowerChar())
+//    println(digit())
+//    println(symbol("!#$^%+@*&_-"))
+//    println(word(5)+word(8)+digit()+digit()+digit())
+    print(dec(0.25,0.25,0.25,0.25)._2)
+
   }
+
+  def dec(f1:Double, f2:Double, f3:Double, f4:Double): ((Double,Double,Double,Double),Int) = {
+      val f = Random.nextDouble();
+
+      def bucket(f : Double): Int = f match {
+        case f if f <= f1                       => 1
+        case f if f > f1 && f <= f1+f2          => 2
+        case f if f > f1+f2 && f <= f1+f2+f3    => 3
+        case _                                  => 4
+    }
+
+    ((f1,f2,f3,f4),bucket(f))
+  }
+
 
   def printableChar():        Char = Random.nextPrintableChar()
   def upperChar():            Char = (Random.nextInt(26) + 65).asInstanceOf[Char]
